2a7200b8414c25535f666ad50bb352463c437a52
Dioraphte <julian.ma@live.nl>
2016-03-24 17:17:58 +0100

    Created Default Files
    Created index.php, style.css, functions.js and added default html to
    index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+  <title>Gustavs.org</title>
+  <meta charset="utf-8" />
+  <link href="style.css" rel="stylesheet" />
+  <script src="functions.js"></script>
+</head>
+<body>
+  
+</body>
+</html>
