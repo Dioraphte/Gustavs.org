39d373139d91ade0329b5a9e2f992a4730984380
Dioraphte <julian.ma@live.nl>
2016-03-24 17:59:40 +0100

    Added Bootstrap
    Added customised bootstrap with glyphicons and carousel

--- index.php
+++ index.php
@@ -2,10 +2,14 @@
 <head>
   <title>Gustavs.org</title>
   <meta charset="utf-8" />
-  <link href="style.css" rel="stylesheet" />
-  <script src="functions.js"></script>
+  <link href="css/style.css" rel="stylesheet" />
+  <link href="css/bootstrap.css" rel="stylesheet" />
+  <script src="js/functions.js"></script>
+  <script src="js/bootstrap.js"></script>
 </head>
 <body>
-  
+  <div class="menu-burger">
+
+  </div>
 </body>
 </html>
