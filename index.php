18f634669232fcdc623470225386f904917b5ecf
Dioraphte <julian.ma@live.nl>
2016-03-24 20:12:59 +0100

    Link fixed

--- index.php
+++ index.php
@@ -3,13 +3,12 @@
   <title>Gustavs.org</title>
   <meta charset="utf-8" />
   <link href="css/style.css" rel="stylesheet" />
-  <link href="css/bootstrap.css" rel="stylesheet" />
+  <link href="css/bootstrap.css" />
   <script src="js/functions.js"></script>
   <script src="js/bootstrap.js"></script>
 </head>
 <body>
-  <div class="menu-burger">
-
+  <div class="main-page">
   </div>
 </body>
 </html>
