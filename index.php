b5445e1daff4e8af1c506e8aa74f4f23723f49e7
Dioraphte <julian.ma@live.nl>
2016-03-24 20:16:58 +0100

    Eh nu pas fixed

--- index.php
+++ index.php
@@ -3,11 +3,14 @@
   <title>Gustavs.org</title>
   <meta charset="utf-8" />
   <link href="css/style.css" rel="stylesheet" />
-  <link href="css/bootstrap.css" />
+  <link href="css/bootstrap.css" rel="stylesheet"/>
   <script src="js/functions.js"></script>
   <script src="js/bootstrap.js"></script>
 </head>
 <body>
+  <div class="burger-menu">
+    <span class="glyphicon glyphicon-menu-hamburger"></span>
+  </div>
   <div class="main-page">
   </div>
 </body>
