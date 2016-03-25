5775439cd512e7a13e3c6b341a3c3a853acb9911
Dioraphte <julian.ma@live.nl>
2016-03-25 12:31:28 +0100

    Made some progress to sidebar
    It now slides, but it needs to slide back

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,15 @@
-$(document).ready(function() {
-  $(".burger").click(function() {
-    $("body").css("background-color", "red");
+$(document).ready(function(){
+  $("#trigger").click(function(){
+    $(".burger-stripes").toggleClass("glyphicon glyphicon-menu-hamburger");
+    $(".burger-stripes").toggleClass("glyphicon glyphicon-remove");
   });
+  $("#trigger").click(function() {
+    $("#menu").toggleClass("active");
+    $("#trigger").animate({
+      marginLeft: "20vw"
+    }, 500);
+  });
+  $(".slided").click(function() {
+    $("#trigger").hide();
+  })
 });
