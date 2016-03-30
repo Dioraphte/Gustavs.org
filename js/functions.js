5b7b16e05bddf67260d19ea037fdca3e10c4e696
Dioraphte <julian.ma@live.nl>
2016-03-30 20:24:24 +0200

    Webpage is done
    Maybe some css can be done and texts can be filled in maybe some images

--- js/functions.js
+++ js/functions.js
@@ -3,19 +3,30 @@ $(document).ready(function() {
     $(".burger-stripes").toggleClass("glyphicon glyphicon-menu-hamburger");
     $(".burger-stripes").toggleClass("glyphicon glyphicon-remove");
   });
+  // $("#trigger").click(function() {
+  //   $("#menu").toggleClass("active");
+  // });
   $("#trigger").click(function() {
-    $("#menu").toggleClass("active");
+    if ( $("#menu").hasClass("menued") === false) {
+      $("#menu").animate({marginLeft: "0"});
+      $("#menu").addClass("menued");
+      $("#menu").css("box-shadow", "5px 0 20px black");
+    } else {
+      $("#menu").animate({marginLeft: "-15vw"});
+      $("#menu").removeClass("menued");
+      $("#menu").css("box-shadow", "0 0 0 #fff");
+    }
   });
+
   $(function() {
     $("#trigger").click(function() {
       if ( $("#trigger").hasClass("triggered") === false) {
         $("#trigger").animate({marginLeft: "22vw"});
-        $("#trigger").toggleClass("triggered");
+        $("#trigger").addClass("triggered");
       } else {
         $("#trigger").animate({marginLeft: "1.5vw"}, 500);
         $("#trigger").removeClass("triggered");
       }
     });
   });
-
 });
